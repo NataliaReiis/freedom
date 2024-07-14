b8e4ebe269fc8174f843c708e76e44558accfc00
NataliaReiis <nataliareis208@gmail.com>
2024-07-14 16:53:01 -0300

    feat: create images contance

---
+++ src/constants/Images.js
@@ -0,0 +1,9 @@
+import assetOne from "@/assets2/pana.png";
+import imageWelcome from "@/assets2/image-welcome.png"
+
+const images =  {
+  "asset-one": assetOne,
+  "image-welcome": imageWelcome
+};
+
+export default images;
