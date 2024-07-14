5bd45b8b8f25dbda9da957f3579b35c43284b55e
NataliaReiis <nataliareis208@gmail.com>
2024-07-14 16:56:02 -0300

    feat: include routes layout app

---
+++ src/app/login/_layout.tsx
@@ -0,0 +1,24 @@
+import { Stack } from "expo-router";
+
+export default function LoginLayout() {
+  return (
+    <Stack>
+      <Stack.Screen
+        name="index"
+        options={{ title: "index", headerShown: false }}
+      />
+      <Stack.Screen
+        name="login-initial"
+        options={{ title: "Login", headerShown: false }}
+      />
+      <Stack.Screen
+        name="forget-password"
+        options={{ title: "Esqueceu a senha" }}
+      />
+      <Stack.Screen
+        name="reset-password"
+        options={{ title: "Resetar senha" }}
+      /> 
+    </Stack>
+  );
+}
