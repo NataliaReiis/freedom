f5422a6f3ae887e9e61d36bb09f915c342aa023e
NataliaReiis <nataliareis208@gmail.com>
2025-02-09 06:11:41 -0300

    feat: add estilo de botao

---
+++ src/ui/styles/button-style.ts
@@ -0,0 +1,10 @@
+import { StyleSheet } from "react-native";
+
+export const buttonStyle = StyleSheet.create({
+  button: {
+    backgroundColor: "#929292",
+    height: 40,
+    justifyContent: "center",
+    textAlign: "center",
+  },
+});
