2b23c7e66c68683da09b8d372eba575fb3b6cc21
NataliaReiis <nataliareis208@gmail.com>
2025-02-09 06:12:04 -0300

    fix: add variaveis de imagens

--- src/ui/constants/Images.js
+++ src/ui/constants/Images.js
@@ -1,9 +1,13 @@
 import assetOne from "../assets/pana.png";
 import imageWelcome from "../assets/image-welcome.png";
+import verifyEmail from "../assets/forgot-password-one.png";
+import resetPassword from "../assets/forgot-password-two.png";
 
-const Images = {
+const images = {
   "asset-one": assetOne,
   "image-welcome": imageWelcome,
+  "verify-email": verifyEmail,
+  "reset-password": resetPassword,
 };
 
-export default Images;
+export default images;
