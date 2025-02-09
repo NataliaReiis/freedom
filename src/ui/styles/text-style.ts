572c47e4d42ab349eea8292970f386677b471c06
NataliaReiis <nataliareis208@gmail.com>
2025-02-09 06:10:56 -0300

    feat: add stlyle de textos

---
+++ src/ui/styles/text-style.ts
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native";
+import { Colors } from "../constants/Colors";
+
+export const textStyle = StyleSheet.create({
+  h1: {
+    fontSize: 40,
+    fontWeight: "500",
+    color: "#13445A",
+    marginBottom: 15,
+  },
+  p: {
+    color: Colors.secondaryGray,
+    fontSize: 14,
+  },
+});
